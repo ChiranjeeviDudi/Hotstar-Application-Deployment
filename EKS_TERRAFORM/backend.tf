9058bb9a7402fe6327b6c517eff7b8509694957a
Chiranjeevi Dudi <125889109+ChiranjeeviDudi@users.noreply.github.com>
2024-06-10 08:07:29 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraformstoringbucket" # Replace with your actual S3 bucket name
+    bucket = "terraform-files-storage-bucket" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
